d346e64767789013f0271bfe8ea51dcae82a73b0
ChilG <zonda037@gmail.com>
2022-11-24 08:37:33 +0700

    delete entities and dto

--- ecu-shop-ev-charger-api-service/src/ocpp/dto/update-ocpp.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateOcppDto } from './create-ocpp.dto';
-
-export class UpdateOcppDto extends PartialType(CreateOcppDto) {
-  id: number;
-}
