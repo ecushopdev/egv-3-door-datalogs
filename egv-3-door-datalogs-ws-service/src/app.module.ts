438c41a231754c0e42d9e33b2d3ecf77a9bf691a
ChilG <zonda037@gmail.com>
2022-12-10 13:59:51 +0700

    remove .env

--- egv-3-door-datalogs-ws-service/src/app.module.ts
+++ egv-3-door-datalogs-ws-service/src/app.module.ts
@@ -2,13 +2,35 @@ import { Module } from '@nestjs/common';
 import { WsModule } from './ws/ws.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DatalogModule } from './datalog/datalog.module';
+import { ConfigModule } from '@nestjs/config';
+import fs from 'fs';
+
+const sslCA = fs.readFileSync('./keys/rds-combined-ca-bundle.pem');
+
+const prdOption = {
+  replicaSet: 'rs0',
+  readPreference: 'secondaryPreferred',
+  ssl: true,
+  useNewUrlParser: true,
+  sslValidate: true,
+  sslCA: sslCA,
+};
+
+let options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+if (process.env.NODE_ENV === 'production') {
+  options = { ...options, ...prdOption };
+}
+
+console.log(options);
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/egv-3-door', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL, options),
     WsModule,
     DatalogModule,
   ],
