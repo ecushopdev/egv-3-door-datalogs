409bd9f1ea5734ad329b2f1826525d5623f820f1
jrweqlop <herher.haha@windowslive.com>
2022-12-12 11:21:09 +0700

    update web-egv-3-door

--- egv-3-door-web/next.config.js
+++ egv-3-door-web/next.config.js
@@ -1,21 +1,7 @@
-const { redirect } = require('next/dist/server/api-utils')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = {
-  async redirect() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: false,
-      }
-    ]
-  },
-  nextConfig
-}
-
+module.exports = nextConfig
